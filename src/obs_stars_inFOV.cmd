7bb21c1508a9c454a750a312525c08c8dec36bd2
wakotaki <48002391+wakotaki@users.noreply.github.com>
2022-10-09 18:00:18 +0900

    update cmd

--- src/obs_stars_inFOV.cmd
+++ src/obs_stars_inFOV.cmd
@@ -1,28 +1,20 @@
-for /l %%a in (2,1,10) do (
-    for /l %%b in (0,1,10) do (
-        for /l %%c in (0,1,4) do (
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 0 --theta_FOV 0
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 0 --theta_FOV 1
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 0 --theta_FOV 2
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 0 --theta_FOV 3
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 1 --theta_FOV 0
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 1 --theta_FOV 1
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 1 --theta_FOV 2
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 1 --theta_FOV 3
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 2 --theta_FOV 0
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 2 --theta_FOV 1
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 2 --theta_FOV 2
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 2 --theta_FOV 3
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 3 --theta_FOV 0
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 3 --theta_FOV 1
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 3 --theta_FOV 2
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 3 --theta_FOV 3
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 4 --theta_FOV 0
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 4 --theta_FOV 1
-            start python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 4 --theta_FOV 2
-            start /wait python subgraph_monte.py --n_obs %%a --theta_img %%b --k %%c --Vmax 4 --theta_FOV 3
-        )
-    )
-)
-
-
+start python obs_stars_inFOV.py --Vmax 0 --theta_FOV 0
+start python obs_stars_inFOV.py --Vmax 1 --theta_FOV 0
+start python obs_stars_inFOV.py --Vmax 2 --theta_FOV 0
+start python obs_stars_inFOV.py --Vmax 3 --theta_FOV 0
+start python obs_stars_inFOV.py --Vmax 4 --theta_FOV 0
+start python obs_stars_inFOV.py --Vmax 0 --theta_FOV 1
+start python obs_stars_inFOV.py --Vmax 1 --theta_FOV 1
+start python obs_stars_inFOV.py --Vmax 2 --theta_FOV 1
+start python obs_stars_inFOV.py --Vmax 3 --theta_FOV 1
+start python obs_stars_inFOV.py --Vmax 4 --theta_FOV 1
+start python obs_stars_inFOV.py --Vmax 0 --theta_FOV 2
+start python obs_stars_inFOV.py --Vmax 1 --theta_FOV 2
+start python obs_stars_inFOV.py --Vmax 2 --theta_FOV 2
+start python obs_stars_inFOV.py --Vmax 3 --theta_FOV 2
+start python obs_stars_inFOV.py --Vmax 4 --theta_FOV 2
+start python obs_stars_inFOV.py --Vmax 0 --theta_FOV 3
+start python obs_stars_inFOV.py --Vmax 1 --theta_FOV 3
+start python obs_stars_inFOV.py --Vmax 2 --theta_FOV 3
+start python obs_stars_inFOV.py --Vmax 3 --theta_FOV 3
+start python obs_stars_inFOV.py --Vmax 4 --theta_FOV 3
