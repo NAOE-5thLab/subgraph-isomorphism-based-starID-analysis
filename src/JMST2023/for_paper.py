12372e7e848e76789ef6556c5541396530e79db6
wakotaki <48002391+wakotaki@users.noreply.github.com>
2023-01-19 17:34:50 +0900

    Update for_paper.py

--- src/JMST2023/for_paper.py
+++ src/JMST2023/for_paper.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+from database import YaleStarCatalog, StarCatalog, InterAngleCatalog
+from utils.unit_convert import deg2rad
 from utils.font import font_setting
 
 
@@ -75,8 +77,19 @@ patterns = [
 # sys
 log_dir = './log/'
 
+DPI = 200
+FIG_SIZE = (6, 2.7)
+marker = 'o'
+font_setting()
+
 
 def main():
+    plot_inFOV()
+    plot_matching()
+    plot_db()
+
+
+def plot_inFOV():    
     logs = []
     for pattern in patterns:
         # param
@@ -94,12 +107,7 @@ def main():
         #
         logs.append(log)
     df = pd.DataFrame(logs)
-    # plot
-    font_setting()
-    DPI = 200
-    FIG_SIZE = (8, 3.5)
-    marker = 'o'
-
+    ### plot ###
     fig = plt.figure(figsize=FIG_SIZE, dpi=DPI)
     ax1 = fig.add_subplot(131)
     ax2 = fig.add_subplot(132)
@@ -155,7 +163,7 @@ def main():
     fname = 'Nobs_le_4.pdf'
     fig.savefig(log_dir+fname)
 
-
+    ### plot ###
     fig = plt.figure(figsize=FIG_SIZE, dpi=DPI)
     ax1 = fig.add_subplot(131)
     ax2 = fig.add_subplot(132)
@@ -211,54 +219,68 @@ def main():
     fname = 'Nobs_le_10.pdf'
     fig.savefig(log_dir+fname)
 
-    # 
-    fname = 'stats_analy_result.csv'
-    df = pd.read_csv(log_dir+fname)
+
+def plot_matching():
+    logs = []
+    for pattern in patterns:
+        # param
+        FOV_deg = pattern['FOV_deg']
+        obsMv = pattern['obsMv']
+        cover_beta = pattern['beta']
+        log = pattern
+        #
+        fname = f'stats_FOV{FOV_deg}_obsMv{obsMv}_beta{cover_beta}.csv'
+        df = pd.read_csv(log_dir+fname)
+        match = df['match'].to_numpy()
+        #
+        log['match_prob'] = match.mean()
+        logs.append(log)
+    df = pd.DataFrame(logs)
 
     fig = plt.figure(figsize=FIG_SIZE, dpi=DPI)
     ax1 = fig.add_subplot(131)
     ax2 = fig.add_subplot(132)
     ax3 = fig.add_subplot(133)
 
-    df_temp = df[df['obsMv'] == 3.5][df['cover_beta'] == 0.0]
+    df_temp = df[df['obsMv'] == 3.5][df['beta'] == 0.0]
     ax1.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(0/5), label='$\\beta = 0.0$')
-    df_temp = df[df['obsMv'] == 3.5][df['cover_beta'] == 0.2]
+    df_temp = df[df['obsMv'] == 3.5][df['beta'] == 0.2]
     ax1.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(1/5), label='$\\beta = 0.2$')
-    df_temp = df[df['obsMv'] == 3.5][df['cover_beta'] == 0.4]
+    df_temp = df[df['obsMv'] == 3.5][df['beta'] == 0.4]
     ax1.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(2/5), label='$\\beta = 0.4$')
-    df_temp = df[df['obsMv'] == 3.5][df['cover_beta'] == 0.6]
+    df_temp = df[df['obsMv'] == 3.5][df['beta'] == 0.6]
     ax1.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(3/5), label='$\\beta = 0.6$')
-    df_temp = df[df['obsMv'] == 3.5][df['cover_beta'] == 0.8]
+    df_temp = df[df['obsMv'] == 3.5][df['beta'] == 0.8]
     ax1.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(4/5), label='$\\beta = 0.8$')
     ax1.set_title('$M_{v} = 3.5$')
     ax1.set_xlabel('FOV [deg.]')
     ax1.set_ylabel('Matching Probability')
     ax1.set_ylim(-0.05, 1.05)
 
-    df_temp = df[df['obsMv'] == 4.5][df['cover_beta'] == 0.0]
+    df_temp = df[df['obsMv'] == 4.5][df['beta'] == 0.0]
     ax2.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(0/5), label='$\\beta = 0.0$')
-    df_temp = df[df['obsMv'] == 4.5][df['cover_beta'] == 0.2]
+    df_temp = df[df['obsMv'] == 4.5][df['beta'] == 0.2]
     ax2.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(1/5), label='$\\beta = 0.2$')
-    df_temp = df[df['obsMv'] == 4.5][df['cover_beta'] == 0.4]
+    df_temp = df[df['obsMv'] == 4.5][df['beta'] == 0.4]
     ax2.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(2/5), label='$\\beta = 0.4$')
-    df_temp = df[df['obsMv'] == 4.5][df['cover_beta'] == 0.6]
+    df_temp = df[df['obsMv'] == 4.5][df['beta'] == 0.6]
     ax2.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(3/5), label='$\\beta = 0.6$')
-    df_temp = df[df['obsMv'] == 4.5][df['cover_beta'] == 0.8]
+    df_temp = df[df['obsMv'] == 4.5][df['beta'] == 0.8]
     ax2.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(4/5), label='$\\beta = 0.8$')
     ax2.set_title('$M_{v} = 4.5$')
     ax2.set_xlabel('FOV [deg.]')
     # ax2.set_ylabel('Matching Probability')
     ax2.set_ylim(-0.05, 1.05)
 
-    df_temp = df[df['obsMv'] == 5.5][df['cover_beta'] == 0.0]
+    df_temp = df[df['obsMv'] == 5.5][df['beta'] == 0.0]
     ax3.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(0/5), label='$\\beta = 0.0$')
-    df_temp = df[df['obsMv'] == 5.5][df['cover_beta'] == 0.2]
+    df_temp = df[df['obsMv'] == 5.5][df['beta'] == 0.2]
     ax3.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(1/5), label='$\\beta = 0.2$')
-    df_temp = df[df['obsMv'] == 5.5][df['cover_beta'] == 0.4]
+    df_temp = df[df['obsMv'] == 5.5][df['beta'] == 0.4]
     ax3.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(2/5), label='$\\beta = 0.4$')
-    df_temp = df[df['obsMv'] == 5.5][df['cover_beta'] == 0.6]
+    df_temp = df[df['obsMv'] == 5.5][df['beta'] == 0.6]
     ax3.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(3/5), label='$\\beta = 0.6$')
-    df_temp = df[df['obsMv'] == 5.5][df['cover_beta'] == 0.8]
+    df_temp = df[df['obsMv'] == 5.5][df['beta'] == 0.8]
     ax3.plot(df_temp['FOV_deg'], df_temp['match_prob'], marker=marker, color = cm.viridis(4/5), label='$\\beta = 0.8$')
     ax3.set_title('$M_{v} = 5.5$')
     ax3.set_xlabel('FOV [deg.]')
@@ -269,7 +291,84 @@ def main():
     fig.tight_layout()
     fname = 'matching_prob.pdf'
     fig.savefig(log_dir+fname)
+    
+def plot_db():
+    ### db size ###
+    fig = plt.figure(figsize=(4, 2), dpi=DPI)
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    # 
+    data = []
+    for i, FOV_deg in enumerate([20, 40, 60, 80]): 
+        U = 4096
+        limitMv = 5.5
+        FOV = deg2rad(FOV_deg)
+        sigma = np.arctan2(2*np.tan(FOV*0.5), U)
+        epsilon = 2*sigma
+        theta_max = 2*np.arctan2(np.sqrt(2)*np.tan(FOV*0.5), 1)
+        ### Catalog setup ###
+        yale = YaleStarCatalog(log_dir=log_dir)
+        star_ctlg = StarCatalog(yale.get_HR(), yale.get_RA(), yale.get_DE())
+        star_ctlg.filtering_by_visual_magnitude(yale.get_Vmag(), limitMv)
+        star_ctlg.filtering_by_multiple_stars(epsilon)
+        #
+        angle_ctlg = InterAngleCatalog(
+            star_ctlg.get_ID(), star_ctlg.get_RA(), star_ctlg.get_DE(),
+            log_dir=log_dir)
+        angle_ctlg.create_catalog(theta_max=theta_max, use_log=False)
+        angles = angle_ctlg.get_inter_angles()
+        # 
+        data.append([FOV_deg, len(angles)])
+    data = np.array(data)
+    ax1.plot(data[:, 0], data[:, 1], marker=marker, color = cm.viridis(0/5))
+    ax1.set_xlabel('FOV [deg.]')
+    ax1.set_ylabel('Batabase size')
 
+    ### matching time ###
+    t_2_data = {20: [], 40: [], 60: [], 80: []}
+    t_3_data = {20: [], 40: [], 60: [], 80: []}
+    t_4_data = {20: [], 40: [], 60: [], 80: []}
+    for pattern in patterns:
+        # param
+        FOV_deg = pattern['FOV_deg']
+        obsMv = pattern['obsMv']
+        cover_beta = pattern['beta']
+        #
+        fname = f'stats_FOV{FOV_deg}_obsMv{obsMv}_beta{cover_beta}.csv'
+        df = pd.read_csv(log_dir+fname)
+        N_obs = df['N_obs'].to_numpy()
+        time = df['time'].to_numpy()
+        #
+        t_2_data[FOV_deg].append(time[N_obs == 2])
+        t_3_data[FOV_deg].append(time[N_obs == 3])
+        t_4_data[FOV_deg].append(time[N_obs >= 4])
+    
+    logs = []
+    for FOV_deg in [20, 40, 60, 80]:
+        log = {'FOV_deg': FOV_deg}
+        log['t_2'] = np.hstack(t_2_data[FOV_deg]).mean()
+        log['t_3'] = np.hstack(t_3_data[FOV_deg]).mean()
+        log['t_4'] = np.hstack(t_4_data[FOV_deg]).mean()
+        logs.appendlogz(log)
+    df = pd.DataFrame(logs)
+    
+    ax2.plot(df['FOV_deg'], df['t_2'], marker=marker, color = cm.viridis(0/3), label='$N_{\mathrm{obs}} = 2$')
+    ax2.plot(df['FOV_deg'], df['t_3'], marker=marker, color = cm.viridis(1/3), label='$N_{\mathrm{obs}} = 3$')
+    ax2.plot(df['FOV_deg'], df['t_4'], marker=marker, color = cm.viridis(2/3), label='$N_{\mathrm{obs}} \geq 4$')
+    ax2.set_xlabel('FOV [deg.]')
+    ax2.set_ylabel('Runtime')
+    # ax2.set_ylim(-0.05, 1.05)
+
+
+def convert_for_step_plot(bins, pd):
+    x = []
+    y = []
+    for i in range(len(bins)-1):
+        x.append(bins[i])
+        y.append(pd[i])
+        x.append(bins[i+1])
+        y.append(pd[i])
+    return x, y
 
 if __name__ == '__main__':
     main()
